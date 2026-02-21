b3b796b410fd90939a7e42785a706191fb504e94
Aryavart Singh Payal <aryavartsinghpayal@gmail.com>
2026-02-21 23:45:14 +0530

    Added volatility calculations to be added in Data for Regimes

---
+++ src/labeling.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+def add_returns(df: pd.DataFrame)->pd.DataFrame:
+    """Adding Daily %age returns based on closing price"""
+    
+    df=df.copy()
+    #pct_change computes (curr-prev)/prev
+    df["return"]=df["Close"].pct_change()
+    return df
+
+def add_volatility(
+    df:pd.DataFrame,
+    window: int=20,
+    annualize: bool=True
+)-> pd.DataFrame:
+    """
+    Add rolling volatility based on returns
+    parameters:
+        window: number of days in rolling window
+        annualize: scale by square root 252 or not
+    """
+    
+    df=df.copy()
+    
+    #Rolling std dev of return
+    vol=df["return"].rolling(window=window).std()
+    
+    if annualize:
+        vol=vol*(252**0.5)
+        
+    df["volatility"]=vol
+    return df
+
+def add_volatility_regime(
+    df:pd.DataFrame,
+    low_q:float = 0.33,
+    high_q:float = 0.66,
+)-> pd.DataFrame:
+    """Label volatility regimes using quantiles"""
+    
+    df=df.copy()
+    
+    #thresholds
+    low_thr=df["volatility"].quantile(low_q)
+    high_thr=df["volatility"].quantile(high_q)
+    
+    def regime(v):
+        if pd.isna(v):
+            return None
+        elif v<=low_thr:
+            return "Low"
+        elif v<=high_thr:
+            return "Medium"
+        else:
+            return "High"
+    
+    df["vol_regime"]=df["volatility"].apply(regime)
+    return df
